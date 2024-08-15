5cf17dc0b08f6cafb300a6a8977a93b03bf30565
CharlotteYinn <yindeveloper1992@gmail.com>
2024-08-15 18:53:21 +0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI as string, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true 
         });
         console.log('MongoDB connected');
     } catch (error) {
